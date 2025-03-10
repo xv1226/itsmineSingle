7eb9c8d7c40dc3b36ca2bf665c5629d982c2af33
K <xv40101392@gmail.com>
2025-03-11 04:14:36 +0900

    ✨ update(리프레시 토큰 api 수정):엑세스토큰으로 조회해서 재발급

--- src/main/java/com/sparta/itsminesingle/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/itsminesingle/domain/user/controller/UserController.java
@@ -5,13 +5,16 @@ import com.sparta.itsminesingle.domain.user.dto.SignupRequestDto;
 import com.sparta.itsminesingle.domain.user.dto.UserResponseDto;
 import com.sparta.itsminesingle.domain.user.entity.User;
 import com.sparta.itsminesingle.domain.user.service.UserService;
+import com.sparta.itsminesingle.domain.user.utils.UserRole;
 import com.sparta.itsminesingle.global.security.UserDetailsImpl;
 import com.sparta.itsminesingle.global.security.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,10 +31,13 @@ public class UserController {
 
     private final JwtUtil jwtUtil;
     private final UserService userService;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public UserController(UserService userService, JwtUtil jwtUtil) {
+    public UserController(UserService userService, JwtUtil jwtUtil,
+            RedisTemplate<String, String> redisTemplate) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+        this.redisTemplate = redisTemplate;
     }
 
 
@@ -70,13 +76,21 @@ public class UserController {
     }
 
     @PostMapping("/user/refresh")
-    public ResponseEntity<String> refresh(@RequestHeader("RefreshToken") String refreshToken) {
-        if (jwtUtil.validateToken(refreshToken)) {
+    public ResponseEntity<String> refresh(@RequestHeader("Authorization") String Token) {
+/*        if (jwtUtil.validateToken(refreshToken)) {
             String username = jwtUtil.getUserInfoFromToken(refreshToken).getSubject();
             return ResponseEntity.ok(jwtUtil.createAccessToken(username, userService.getUserByUsername(username).getUserRole()));
         } else {
             throw new IllegalArgumentException("유효하지 않은 리프레시 토큰입니다.");
-        }
+        }*/
+        String accessToken = jwtUtil.getTokenWithoutBearer(Token);
+        String username = jwtUtil.getUsernameFromToken(accessToken);
+
+        String refreshToken = jwtUtil.substringToken(redisTemplate.opsForValue().get(username));
+        Claims claims = jwtUtil.getUserInfoFromToken(refreshToken);
+        String newAccessToken = jwtUtil.getTokenWithoutBearer(jwtUtil.createAccessToken(claims.getSubject(), UserRole.valueOf(claims.get(JwtUtil.AUTHORIZATION_KEY).toString())));
+
+        return ResponseEntity.ok(newAccessToken);
     }
 
     @DeleteMapping("/user/delete")
